b201ad1877eb8d9c4f966b161736264e627f05f8
grbeno <grbeno@gmail.com>
2024-07-24 20:37:20 +0200

    update-async-add-filename-gen

--- chat.py
+++ chat.py
@@ -10,7 +10,12 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.messages import AIMessage
 
 from dotenv import load_dotenv
-from llms import Llms
+from colorama import init, Fore, Style
+
+from llms import Llms, ChatContext
+
+# Initialize colorama
+init()
 
 # Load environment variables from .env file
 load_dotenv()
@@ -30,17 +35,18 @@ selected_model = getattr(llm, 'model')
 print(f"Model: {selected_model}")
 
 system_prompt = """ You are helpful, creative, clever, and very friendly assistant. 
-Your response should be short but concise, no more than 5 sentences. """  # Change this role to whatever you want
+Your response should be short but concise, no more than 3 sentences. """  # Change this role to whatever you want
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),
     MessagesPlaceholder(variable_name="history"),
-    ("user", "{input}"),
+    ("human", "{input}"),
 ])
 
 chain = prompt | model | StrOutputParser()
 
 store = {}
+
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
     if session_id not in store:
         store[session_id] = ChatMessageHistory()
@@ -56,34 +62,49 @@ with_message_history = RunnableWithMessageHistory(
 
 # Chat on the command line
 async def chat_loop():
+    
     print("\nI am your assistant. Ask me anything or chat with me.")
+    
     while True:
-        if prompt := input("\nUser: "):
-            config = {"configurable": {"session_id": "chat"}}
+        
+        if prompt := input(f"\n{Fore.YELLOW}{Style.NORMAL}User: "):
+            
+            # Invoke the AI model with the prompt
             response = with_message_history.invoke(
                 {"input": prompt},
-                config=config,
+                config={"configurable": {"session_id": "chat"}},
             )
-            if 'gpt' in selected_model:
-                response = response
-            print(f"AI: ", end='')
-            async for chunk in chain.astream({"input": prompt, "history": list(response)}):
-                print(chunk, end='', flush=True)
+            
+            # Print the AI response
+            print(f"{Fore.GREEN}{Style.NORMAL}AI: ", end='')
+            
+            # Streaming output
+            for char in response:
+                print(char, end='', flush=True)
+                await asyncio.sleep(0.01)  # Adjust this value to control the speed of output
+        
         else:
-            # Ask a question to save or not to text file, if yes then save the conversation to a text file
+            # Ask a question to save or not to text file
             save = input("Do you want to save the conversation to a text file? (y/n): ")
+            
             if save.lower() == 'y':
+                
                 # Make directory if not exists
                 dir = 'conversations'
                 if not os.path.exists(dir):
                     os.makedirs(dir)
-                filename = input("Enter a valid filename (without extension): ")
-                with open(f"{dir}/{filename}.txt", 'w') as f:
+                
+                # Save the conversation to a text file
+                aigenerated_filename =ChatContext(store['chat'].messages).add_filename()
+                
+                with open(f"{dir}/{aigenerated_filename}.txt", 'w', encoding='UTF-8') as f:
                     f.write(f"Model: {selected_model}\n\n")
                     for message in store['chat'].messages:
                         prefix = "AI" if isinstance(message, AIMessage) else "User"
                         f.write(f"{prefix}: {message.content}\n")
-                print("Conversation saved to text file.")
+                
+                print(f"Conversation saved to the file: {aigenerated_filename}.txt")
+            
             break  # Exit the loop if the user enters an empty prompt
 
 # Call the async function to start the chat loop
--- llms.py
+++ llms.py
@@ -1,7 +1,9 @@
 import os
+import json
 
 from langchain_openai import ChatOpenAI
 from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.messages import AIMessage
 
 
 class Llms():
@@ -10,21 +12,21 @@ class Llms():
     def __init__(self, model: str) -> None:
         self.model = model
 
-    def __select_gpt(self) -> callable:
+    def openai(self) -> callable:
         """Selects the model from OpenAI."""
         return ChatOpenAI(
             api_key=os.environ.get("OPENAI_API_KEY"),
             model=self.model,
-            max_tokens=100,
+            max_tokens=200,
             temperature=0.7,
         )
         
-    def __select_hf_opensource(self) -> callable:
+    def hugging_face(self) -> callable:
         """Selects the model from the Hugging Face model hub."""
         return HuggingFaceEndpoint(
             repo_id=self.model,
             task="text-generation",
-            max_new_tokens=100,
+            max_new_tokens=200,
             do_sample=False,
             huggingfacehub_api_token=os.environ.get("HUGGINGFACE_API_KEY"),
         )
@@ -32,7 +34,31 @@ class Llms():
     def get_model(self):
         """Selects the model."""
         if 'gpt' in self.model:
-            return self.__select_gpt()
+            return self.openai()
         else:
-            return self.__select_hf_opensource()
+            return self.hugging_face()
+
+
+class ChatContext(Llms):
+
+    def __init__(self, conversation: dict) -> None:
+        super().__init__('gpt-4o-mini')
+        self.conversation = conversation
+
+    def get_content(self) -> json:
+        res = []
+        for message in self.conversation:
+            prefix = "AI" if isinstance(message, AIMessage) else "User"
+            res.append(f"{prefix}: {message.content}")
+        return json.dumps(res)
+    
+    def add_filename(self) -> str:    
+        create_title = [
+            (
+                "system",
+                "Generate short filename about the prompt, without any extension. The filename should be less than 20 characters. Your response have to be only the filename itself.",
+            ),
+            ("human", self.get_content()),  # should not be list!
+        ]
+        return self.openai().invoke(create_title).content
     
